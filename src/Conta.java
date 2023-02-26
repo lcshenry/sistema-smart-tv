358b7b2d80f7e00dd0d1629a3683f0c15d20e7d6
lcshenry <lucashenrique51513@gmail.com>
2023-02-26 04:48:57 -0300

    interacao

--- src/Conta.java
+++ src/Conta.java
@@ -1,22 +1,40 @@
+ import java.util.Locale;
+ import java.util.Scanner;
 public class Conta {
-    double saldo = 10.0;
 
-    public void sacar (Double valor){
-        double novoSaldo = saldo - valor;
-    }
-    public void imprimirSaldo(){
-        //
-        System.out.println(saldo);
-        //
-        System.out.println(novoSaldo);
-    }
-    public  double calcularDividaExponencial(){
-        double valorParcela = 50.0;
-        double valorMontante = 0.0;
-        for(int x=1; x<=5; x++){
-            double valorCalculado = valorParcela * x;
-            valorMontante = valorMontante + valorCalculado;
-        }
-        return valorMontante;
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        String firstName = "Lucas";
+        String lastName = "Henrique";
+        int actualYear = 2023;
+        int birthday = 1999;
+        int age = actualYear - birthday;
+
+        //Forma diferente para printar nome na tela, porem sem interação;
+        System.out.println("Me chamo" + " " + firstName + " " + lastName);
+        System.out.println("Tenho" + " " + age + " " + "anos");
+
+        /**
+         * 1 -> import java.util.Locale;
+         * 2 -> import java.util.Scanner;
+         * 3 -> Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+         * 4 -> String "nome" = scanner.next();
+         * 5 -> int "nome" = scanner.nextInt();
+         * 6 -> double "nome" = scanner.nextDouble();
+         */
+        //print de forma interativa atraves do scanner
+        System.out.println("Qual o seu nome? :");
+        String name = scanner.next();
+        System.out.println("Qual o seu sobrenome? :");
+        String lastname = scanner.next();
+        System.out.println("Qual a sua idade? :");
+        int years = scanner.nextInt();
+        System.out.println("Qual o ano de nascimento? :");
+        int birth = scanner.nextInt();
+        System.out.println("Tipo de double? :");
+        double tipo = scanner.nextDouble();
+
     }
 }
