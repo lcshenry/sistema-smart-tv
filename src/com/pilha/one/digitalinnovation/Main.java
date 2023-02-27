131da08c5985f8e4864649a3196b7306646c7683
lcshenry <lucashenrique51513@gmail.com>
2023-02-27 12:31:23 -0300

    interacao

---
+++ src/com/pilha/one/digitalinnovation/Main.java
@@ -0,0 +1,17 @@
+package com.pilha.one.digitalinnovation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        pilha minhaPilha = new pilha();
+        minhaPilha.push(new No(1));
+        minhaPilha.push(new No(2));
+        minhaPilha.push(new No(3));
+        minhaPilha.push(new No(4));
+        minhaPilha.push(new No(5));
+        minhaPilha.push(new No(6));
+
+        System.out.println(minhaPilha);
+
+    }
+}
